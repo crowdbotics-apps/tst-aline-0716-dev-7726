babcc036346a5972bbbc225d5debdd1dcf9bdecf
Crowdbotics <team@crowdbotics.com>
2020-07-21 15:48:01 +0000

    Update components

--- src/features/BlankScreen29175/screens/blank.js
+++ src/features/BlankScreen29175/screens/blank.js
@@ -247,7 +247,43 @@ export class _Blank extends React.Component {
         }}
         onPress={() => alert("Pressed!")}
       >
-        Press me!
+        new Color
+      </Button>
+      <Button
+        backgroundColor="#3366FF"
+        style={{
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "center",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#FFFFFF",
+          backgroundColor: "#FFFFFF",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none"
+        }}
+        onPress={() => alert("Pressed!")}
+      >
+        No Color Change
       </Button>
     </View>
   )
